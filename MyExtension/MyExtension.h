efa553393e17e217fbabaf1f02cfdf52d2de1ccc
Yuya Hirayama <y@yura.me>
2017-07-16 13:55:09 +0900

    Initial commit

---
+++ MyExtension/MyExtension.h
@@ -0,0 +1,19 @@
+//
+//  MyExtension.h
+//  MyExtension
+//
+//  Created by Yuya Hirayama on 2017/07/16.
+//  Copyright © 2017年 Yuya Hirayama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyExtension.
+FOUNDATION_EXPORT double MyExtensionVersionNumber;
+
+//! Project version string for MyExtension.
+FOUNDATION_EXPORT const unsigned char MyExtensionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyExtension/PublicHeader.h>
+
+
