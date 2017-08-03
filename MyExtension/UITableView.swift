af26e8f17bdec191dae26c7ed4772390c099f1c8
Yuya Hirayama <y@yura.me>
2017-08-04 03:33:39 +0900

    Improve tableView

--- MyExtension/UITableView.swift
+++ MyExtension/UITableView.swift
@@ -8,16 +8,12 @@
 
 import UIKit
 
-private func cellReuseIdentifier(for type: UITableViewCell.Type) -> String {
-  return String.init(describing: type)
-}
-
 public extension MyExtension where Base: UITableView {
-  func registerCell<Cell: UITableViewCell>(type : Cell.Type, xibName: String = cellReuseIdentifier(for: Cell.self)) {
-    base.register(UINib.init(nibName: xibName, bundle: nil), forCellReuseIdentifier: xibName)
+  func registerCell<Cell: UITableViewCell>(type : Cell.Type) where Cell: XibInstantiatable {
+    base.register(UINib.init(nibName: Cell.xibName, bundle: nil), forCellReuseIdentifier: Cell.xibName)
   }
 
-  func dequeueCell<Cell: UITableViewCell>(at indexPath: IndexPath) -> Cell {
-    return base.dequeueReusableCell(withIdentifier: cellReuseIdentifier(for: Cell.self), for: indexPath) as! Cell
+  func dequeueCell<Cell: UITableViewCell>(at indexPath: IndexPath) -> Cell where Cell: XibInstantiatable {
+    return base.dequeueReusableCell(withIdentifier: Cell.xibName, for: indexPath) as! Cell
   }
 }
